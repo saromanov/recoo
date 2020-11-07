ec6da8d1a953e71f5d04b9a76894cfae9d82ecf3
Sergey <xxsmotur@gmail.com>
2020-11-07 13:37:03 +0500

    Add method for build image

--- internal/recoo/build/docker.go
+++ internal/recoo/build/docker.go
@@ -1,9 +1,16 @@
 package build
 
 import (
+	"archive/tar"
+	"bytes"
+	"context"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/saromanov/recoo/internal/config"
 )
 
@@ -15,7 +22,68 @@ func createDockerfile(cfg config.Build, lang Language) error {
 	return nil
 }
 
+func buildImage(client *client.Client, tags []string, dockerfile string) error {
+	ctx := context.Background()
+
+	buf := new(bytes.Buffer)
+	tw := tar.NewWriter(buf)
+	defer tw.Close()
+
+	dockerFileReader, err := os.Open(dockerfile)
+	if err != nil {
+		return err
+	}
+
+	readDockerFile, err := ioutil.ReadAll(dockerFileReader)
+	if err != nil {
+		return err
+	}
+
+	tarHeader := &tar.Header{
+		Name: dockerfile,
+		Size: int64(len(readDockerFile)),
+	}
+
+	err = tw.WriteHeader(tarHeader)
+	if err != nil {
+		return err
+	}
+
+	_, err = tw.Write(readDockerFile)
+	if err != nil {
+		return err
+	}
+
+	dockerFileTarReader := bytes.NewReader(buf.Bytes())
+
+	buildOptions := types.ImageBuildOptions{
+		Context:    dockerFileTarReader,
+		Dockerfile: dockerfile,
+		Remove:     true,
+		Tags:       tags,
+	}
+
+	imageBuildResponse, err := client.ImageBuild(
+		ctx,
+		dockerFileTarReader,
+		buildOptions,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	defer imageBuildResponse.Body.Close()
+	_, err = io.Copy(os.Stdout, imageBuildResponse.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // generateDockerfile provides generating of Dockerfiloe based on language
+// https://semaphoreci.com/community/tutorials/how-to-deploy-a-go-web-application-with-docker
 func generateDockerfile(cfg config.Build) string {
 	data := fmt.Sprintf("FROM %s\n", cfg.Image)
 	data += fmt.Sprintf("ADD . /app\n")
