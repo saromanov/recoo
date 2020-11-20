0e07553ddbd4e1d968eaee77cad994e297b47ddf
Sergey <xxsmotur@gmail.com>
2020-11-20 23:45:04 +0500

    Add draft for release

--- internal/recoo/deploy/swarm/swarm.go
+++ internal/recoo/deploy/swarm/swarm.go
@@ -1,5 +1,12 @@
 package swarm
 
+import "github.com/saromanov/recoo/internal/deploy"
+
 // Service defines deploy to docker swarm
 type Service struct {
+}
+
+// New provides initialization on swarm
+func New() *deploy.Deploy {
+	return &Service{}
 }
