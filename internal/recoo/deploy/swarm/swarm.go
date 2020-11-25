46a0072f6016df2e478a6f2e3f6b4dd03403a9f3
Sergey <xxsmotur@gmail.com>
2020-11-25 20:13:34 +0500

    Add draft for generate compose file

--- internal/recoo/deploy/swarm/swarm.go
+++ internal/recoo/deploy/swarm/swarm.go
@@ -1,6 +1,11 @@
 package swarm
 
-import "github.com/saromanov/recoo/internal/deploy"
+import (
+	"fmt"
+	"os"
+
+	"github.com/saromanov/recoo/internal/deploy"
+)
 
 // Service defines deploy to docker swarm
 type Service struct {
@@ -12,5 +17,9 @@ func New() deploy.Deploy {
 }
 
 func (s *Service) Run() error {
+	_, err := os.Exec("docker swarm deploy").Output()
+	if err != nil {
+		return fmt.Errorf("unable to exec: %v", err)
+	}
 	return nil
 }
