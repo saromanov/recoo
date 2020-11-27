24812b92ba0ba5d4d73b23446273cac1a3779b1d
Sergey <xxsmotur@gmail.com>
2020-11-27 22:23:06 +0500

    Add generate of compose file

--- internal/recoo/deploy/swarm/generate.go
+++ internal/recoo/deploy/swarm/generate.go
@@ -1,10 +1,30 @@
 package swarm
 
-func generateCompose() string {
-	result := "version: '3.8'\n"
-	result += "network\n:
-			\b\btest\n"
-	result += "services:\n"
-	
-	return result
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/go-yaml/yaml"
+)
+
+// Compose defines file for saving data
+type Compose struct {
+	Version  string   `yaml:"version"`
+	Networks []string `yaml:"networks"`
+}
+
+func generateCompose() error {
+	c := &Compose{
+		Version:  "'3.8'",
+		Networks: []string{"test"},
+	}
+
+	out, err := yaml.Marshal(c)
+	if err != nil {
+		return fmt.Errorf("unable to marshal to file: %v", err)
+	}
+	if err := ioutil.WriteFile("docker-compose.yml", out, 644); err != nil {
+		return fmt.Errorf("unable to write to file: %v", err)
+	}
+	return nil
 }
