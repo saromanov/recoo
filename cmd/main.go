583983ecc55b1b5e18c08f3bf4bd39dc6376727c
Sergey <xxsmotur@gmail.com>
2020-12-03 20:59:12 +0500

    Add pipeline execution

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"context"
-
+	"os"
 	"github.com/saromanov/recoo/internal/config"
 	"github.com/saromanov/recoo/internal/core"
 	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 )
 
-func main() {
+func exec(){
+app := &cli.App{
+	Name:  "mystery",
+	Usage: "Starting of the app",
+	Flags: []cli.Flag{
+		
+	},
+	Commands: []*cli.Command{
+		{
+			Name:   "run",
+			Usage:  "running of pipeline",
+			Action: run,
+		},
+	},
+}
+
+err := app.Run(os.Args)
+if err != nil {
+	return
+}
+}
+
+func run(c *cli.Context) error {
 	cfg, err := config.Load("config.yml")
 	if err != nil {
 		logrus.WithError(err).Fatalf("unable to load config")
@@ -21,5 +44,8 @@ func main() {
 	if err := c.Start(context.Background()); err != nil {
 		logrus.WithError(err).Fatalf("unable to execute pipeline")
 	}
-
+	return nil
+}
+func main() {
+	exec()
 }
